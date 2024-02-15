1593628d0d700266f3b3afed087f6a74289b3ddc
kasmil <kamilsalikhov75@gmail.com>
2024-02-16 01:59:35 +0300

    fix build

--- src/shared/api/strapi/index.ts
+++ src/shared/api/strapi/index.ts
@@ -2,7 +2,10 @@ import { API } from "../instance";
 import Cookies from "js-cookie";
 
 export const strapi = new API({
-  baseUrl: import.meta.env.STRAPI_API_URL || "http://localhost:1337/api",
+  baseUrl:
+    import.meta.env.STRAPI_API_URL ||
+    "https://inviting-wealth-c19c9dc6be.strapiapp.com/api" ||
+    "http://localhost:1337/api",
 });
 
 export const getHeaders = () => {
